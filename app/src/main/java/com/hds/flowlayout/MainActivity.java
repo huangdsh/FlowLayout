91b0a8a4ec6f5dd3ddb051730b7f8e80e6783b92
hds <185469580@qq.com>
2017-03-14 10:28:08 +0800

    基本Demo

--- app/src/main/java/com/hds/flowlayout/MainActivity.java
+++ app/src/main/java/com/hds/flowlayout/MainActivity.java
@@ -1,13 +1,47 @@
 package com.hds.flowlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private String mNames[] = {
+            "welcome", "android", "TextView",
+            "apple", "jamy", "kobe bryant",
+            "jordan", "layout", "viewgroup",
+            "margin", "padding", "text",
+            "name", "type", "search", "logcat"
+    };
+    private FlowViewGroup mFlowLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initChildViews();
+
+    }
+
+    private void initChildViews() {
+        // TODO Auto-generated method stub
+        mFlowLayout = (FlowViewGroup) findViewById(R.id.flowlayout);
+        MarginLayoutParams lp = new MarginLayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        lp.leftMargin = 5;
+        lp.rightMargin = 5;
+        lp.topMargin = 5;
+        lp.bottomMargin = 5;
+        for (int i = 0; i < mNames.length; i++) {
+            TextView view = new TextView(this);
+            view.setText(mNames[i]);
+            view.setTextColor(Color.WHITE);
+            view.setBackgroundDrawable(getResources().getDrawable(R.drawable.textview_bg));
+            mFlowLayout.addView(view, lp);
+        }
     }
 }
